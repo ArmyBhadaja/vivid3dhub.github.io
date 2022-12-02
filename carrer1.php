d0fe8933aafcff6f889cb81f22cb61ac6c69ae13
ArmyBhadaja <119677410+ArmyBhadaja@users.noreply.github.com>
2022-12-02 19:40:14 +0530

    Add files via upload
    Version_1

---
+++ carrer1.php
@@ -0,0 +1,35 @@
+<!-- INSERT INTO `carrer` (`name`, `email`, `contact`, `msg`) VALUES ('Army bhadaja', 'army@gmail.com', '9328573535', 'My mother is my my world'); -->
+
+
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+     
+     $server = "localhost";
+     $username = "root";
+     $password = "";    
+
+     $con = mysqli_connect($server, $username, $password);
+
+        if(!$con){
+            die("connection to this database failed due to" . mysqli_connect_error());
+        }
+        // echo "Success connecting to the db";
+
+         $name  =$_POST['name'];
+         $email =$_POST['email'];   
+         $phone =$_POST['phone'];   
+         $msg  =$_POST['msg'];  
+         $sql ="INSERT INTO `vivide`.`carrer` ( `name`, `email`, `phone`, `msg`) VALUES ( '$name', '$email', '$phone', '$msg');";
+        // echo $sql;
+         if($con->query($sql) == true){
+         //   echo "Successfully inserted";
+           $insert=true;
+        }
+        else{
+            echo "ERROR:$sql <br> $con->error";
+        }
+
+            $con->close();
+    }
+         ?>
